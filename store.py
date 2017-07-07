9e9e263e987dd52498f623cdb615dabd2d7e7041
John O. Paul <oyomthojohnpaul@gmail.com>
2017-07-07 12:58:06 +0300

    modify internal data structure

--- store.py
+++ store.py
@@ -2,17 +2,31 @@
 This module is for storing and manipulating data
 """
 
-class Db():
+class Store():
     def __init__(self):
         self.__data = dict()
     
-    def add_topic(self, slug, name):
-        self.__data[slug] = dict(name=name, skills=list())
+    def add_topic(self, topic):
+        """adds a new topic"""
+        self.__data[topic] = list()
     
-    def add_skill(self, slug, name):
-        skill = dict(name=name, is_complete=False)
-        self.__data[slug]["skills"].append(skill)
+    def add_skill(self, topic, skill_name):
+        """adds a new skill """
+        skill = dict(name=skill_name, is_complete=False)
+        self.__data[topic].append(skill)
     
     def total(self):
         return len(self.__data)
+
+    def print(self): # junk method
+        """for debugging"""
+        print(self.__data)
+
     
+    def get_topics(self):
+        """returns a list of topics"""
+        topics = []
+
+        for topic in self.__data:
+            topics.append(topic.name)
+        return topics;
--- storetest.py
+++ storetest.py
@@ -1,22 +1,23 @@
-import unittest, db
+import unittest, store
 
 class Test(unittest.TestCase):
 
     def test_add_topic(self):
-        _db = db.Db()
+        _store = store.Store()
 
         slug = "java-programming"
         name = "Java programming"
-        _db.add_topic(slug, name)
+        _store.add_topic(slug, name)
 
-        self.assertEqual(1, _db.total())
+        self.assertEqual(1, _store.total())
 
         slug = "css-flexbox"
         name = "CSS flexbox"
-        _db.add_topic(slug, name)
+        _store.add_topic(slug, name)
 
-        self.assertEqual(3, _db.total())
-    
+        self.assertEqual(2, _store.total())
+        _store.print()
+
     def test_add_skill(self):
          slug = "java-programming"
          name = "Hibernate"
