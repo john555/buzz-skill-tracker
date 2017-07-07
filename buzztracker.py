ce66da6232df19787ca85ee0b4f8fca651f89cca
Anyama Richard <anyamaronyango@gmail.com>
2017-07-07 14:38:24 +0300

    updating skill_completed function

--- buzztracker.py
+++ buzztracker.py
@@ -13,8 +13,27 @@ class BuzzTracker(object):
         # eg, skills['python] = ['TDD', 'dictionaries', 'lists']
         #skills becomes skills = {'python': ['TDD', 'dictionaries', 'lists'], .... and others}
 
-    def skills_studied(self):
-    
+    def skills_completed(self,skills):
+        """ Function to check and mark completed skills """
+        print('Please enter Yes/No for skills you have studied:')
+        for topics, skill in skills.items:
+            for value in skill:
+                user_input = input(value)
+                if user_input == 'Yes': 
+                    self.skills_studied[topics] = value
+                else:
+                    pass
+        
+        return skills_studied
+
+       
+
+    def view_skills_studied(self,skills):
+        pass
+
+    def view_skills_not_studied(self,skills):
+        pass
+
         pass
 
     def view_skills_studied(self):
@@ -55,6 +74,8 @@ class BuzzTracker(object):
         else:
             self.skills[topic] = []
 
+<<<<<<< HEAD
+=======
     def view_topics_added (self) :
         topics = self.skills
 
@@ -63,16 +84,16 @@ class BuzzTracker(object):
 
 if __name__ == "__main__":
     track =  BuzzTracker()
-    print (10 * '-',"Welcome to the Buzz project Tracker.",10*'-' ) #updated the menu to work appropriately.
+    print (30 * '-')
     print ('Enter: ')
     print ('0 to Add a topic ')
     print ('1.add skills to the tracker ')
     print ('2.show skills that are studied ')
     print ('3. view skills not studied')
     print ('4. Show the Progress of the skills.')
-    print ('5. View topics added.')
-    print ('6. Exit the menu.')
-    choice = int(input('Enter the choice from the menu: 1-5: ')) #updated the input value to return an int.
+    print ('5. Exit the menu.')
+    choice = input('Enter the choice from the menu: 1-5')
+>>>>>>> 219d9f417bec1c8d778c7ad36399168f8bc7df0b
 
     if choice == 0:
         track.add_topic()
@@ -88,15 +109,10 @@ if __name__ == "__main__":
     elif choice == 4:
         track.progress()
 
-    elif choice == 5:
-        track.view_topics_added()       #the numbering of the files was off just updated that.
-    
-    elif choice==6:
-        print('Print exiting the menu')
-        exit()
-
+    elif choice == 6:
+        print('To exit, press ctr + c ')
     else:
-        print("Input a correct number.")
+        print ("Wrong input. Enter one of the options to try again.")
 
 
 
