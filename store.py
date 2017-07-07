8df82ca92272ab727959e1831eb180cb9d0ac573
John O. Paul <oyomthojohnpaul@gmail.com>
2017-07-07 13:17:14 +0300

    add methods to mark skills as complete

--- store.py
+++ store.py
@@ -5,24 +5,30 @@ This module is for storing and manipulating data
 class Store():
     def __init__(self):
         self.__data = dict()
-    
+
     def add_topic(self, topic):
         """adds a new topic"""
         self.__data[topic] = list()
-    
+
     def add_skill(self, topic, skill_name):
         """adds a new skill """
         skill = dict(name=skill_name, is_complete=False)
         self.__data[topic].append(skill)
     
+    def get_skill(self, topic, index):
+        return self.__data[topic][index]
     def total(self):
         return len(self.__data)
+
+    def set_complete(self, topic, index):
+        """marks a skill as complete. index is the index of the skill in the list"""
+        self.__data[topic][index]['is_complete'] = True
 
     def print(self): # junk method
         """for debugging"""
         print(self.__data)
 
-    
+
     def get_topics(self):
         """returns a list of topics"""
         topics = []
--- storetest.py
+++ storetest.py
@@ -4,18 +4,20 @@ class Test(unittest.TestCase):
 
     def test_add_topic(self):
         _store = store.Store()
-
-        slug = "java-programming"
         name = "Java programming"
-        _store.add_topic(slug, name)
+        _store.add_topic(name)
 
         self.assertEqual(1, _store.total())
 
-        slug = "css-flexbox"
-        name = "CSS flexbox"
-        _store.add_topic(slug, name)
+        name = "CSS"
+        _store.add_topic(name)
 
         self.assertEqual(2, _store.total())
+
+        _store.add_skill("CSS", "Flex box")
+        
+        _store.print()
+        _store.set_complete("CSS", 0)
         _store.print()
 
     def test_add_skill(self):
